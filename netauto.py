f11285dbe8c4c03b1181d66aa285231ae749b56f
iribarrem <gustavo.iribarrem@gmail.com>
2024-02-21 22:45:18 -0300

    implemented: route-policies, community-filters

--- netauto.py
+++ netauto.py
@@ -9,7 +9,8 @@ def configure_bgp(host, netbox) -> str:
     bgp = netbox.plugins.bgp
 
     config += configure_prefix_lists(host, bgp)
-
+    config += configure_communities(host, bgp)
+    config += configure_route_policies(host, bgp)
     return config
 
 def configure_prefix_lists(host, bgp) -> str:
@@ -41,7 +42,83 @@ def configure_prefix_lists(host, bgp) -> str:
                 config += " less-equal " + str(rule.le)
 
             config += "\n"
-    return config
+
+    return config + "\n"
+
+def configure_route_policies(host, bgp) -> str:
+    output: str = ""
+
+    for bgp_session in bgp.session.filter(device=host.name):
+        routing_policies = bgp_session.import_policies + bgp_session.export_policies
+        for routing_policy in routing_policies:
+            rules = bgp.routing_policy_rule.filter(routing_policy_id=routing_policy.id)
+            for rule in rules:
+                output += f"route-policy {routing_policy.name} {rule.action} node {rule.index}\n"
+
+                if rule.description:
+                    output += f"    description {rule.description}\n"
+
+                ## Match Clauses ------
+                for prefix_list in rule.match_ip_address:
+                    output += f"    if-match ip-prefix {prefix_list.name}\n"
+
+                for community in rule.match_community:
+                    community_filter = bgp.community.get(id=community.id).description
+                    output += f"    if-match community-filter {community_filter}\n"
+                
+                if rule.match_custom:
+                    for match_clause in rule.match_custom:
+                        if match_clause[0] == 'as-path-filter':
+                            output += f"    if-match as-path-filter {match_clause[1]}\n"
+
+                        elif match_clause[0] == 'acl':
+                            output += f"    if-match acl {match_clause[1]}\n"
+                        
+                        else:
+                            raise Exception(f"Custom Match Clause not supported: {match_clause[0]}")
+
+                ## Apply Clauses -------
+                if rule.set_actions:
+                    for action in rule.set_actions:
+                        if action[0] == 'community':
+                            output += "    apply community "
+                            for community in action[1]:
+                                output += f"{community} "
+                            output += "\n"
+                        
+                        elif action[0] == 'as-path prepend':
+                            output += "    apply as-path "
+                            for asn in action[1]:
+                                output += f"{asn} "
+                            output += "additive\n"
+
+                        elif action[0] == 'next-hop':
+                            output += f"    apply ip-address next-hop {action[1]}\n"
+                
+                        else:
+                            raise Exception(f"Custom Apply Clause not supported: {action[0]}")
+                    # ------ TODO: CUSTOM APPLIES > ? ------ #
+                
+                if rule.custom_fields["local_preference"]:
+                    output += f'    apply local-preference {rule.custom_fields["local_preference"]}\n'
+
+            output += "\n"  
+    return output
+
+def configure_communities(host, bgp) -> str:
+    output: str = ""
+
+    for bgp_session in bgp.session.filter(device=host.name):
+        routing_policies = bgp_session.import_policies + bgp_session.export_policies
+        for routing_policy in routing_policies:
+            rules = bgp.routing_policy_rule.filter(routing_policy_id=routing_policy.id)
+            for rule in rules:
+                for community in rule.match_community:
+                    community_filter = bgp.community.get(id=community.id)
+                    output += f"ip community-filter basic {community_filter.description} index 10 permit {community_filter.value}\n"
+
+    output += "\n"
+    return output
 
 def main():
     logger = logging.getLogger("logger")
@@ -59,8 +136,10 @@ def main():
 
     for host in netbox.dcim.devices.all():
         output_config = ""
+
         if host.primary_ip and host.platform:
             if host.platform.name == "Huawei VRP":
+                print(f"Generating configuration script for {host.name}...\n\n")
                 # output_config += HUAWEI_INITIAL_DEFAULT_CONFIG
 
                 output_config += configure_bgp(host, netbox)
